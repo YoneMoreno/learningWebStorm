ae0bafad1e22b7307d30298fe255ea49fde01d1e
YonePC <YonePC@YONE>
2017-10-07 19:01:44 +0100

    Adding views: created a directory to put our index.html renaming it with shift f6 to home.handlebars to use a view engine called mouestache throught the express-handlebars package. Also we pass in a title and message via the engine

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var http = require("http");
 var express = require("express");
 var app = express();
+var hb = require("express-handlebars");
 
 app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", hb());
+app.set("view engine", "handlebars");
+
+app.get("/", function (req, res) {
+    res.render("home", { title: "Hello from WebStorm", message: "Hi there!" });
+});
 
 var server = http.createServer(app);
 
