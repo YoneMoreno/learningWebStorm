c16e1f505142b6e92cf80f720abf43cfc14e65ca
YonePC <YonePC@YONE>
2017-10-07 09:58:10 +0100

    Using language injection: highlighting a string and pressing alt enter lets edit the html in a window. Emmet (li>lorem3)*5 creates five list items which each one have 3 words. It lets edit js too. It lets check regexp.

--- PizzaShop/js/index.js
+++ PizzaShop/js/index.js
@@ -26,4 +26,11 @@
     dog.legCount = 3;
     dog.feed("Fish");
 
+    var toInsert ="<div>\n    <ul>\n        <li>Lorem ipsum dolor.</li>\n        <li>Blanditiis, explicabo, tempora.</li>\n        <li>Commodi, distinctio, labore?</li>\n        <li>Hic, obcaecati, ratione.</li>\n        <li>Id, incidunt, temporibus.</li>\n    </ul>\n</div>"
+
+    // language=JavaScript
+    var toExecute = "var x = 0;\nx++;\nx = 5;";
+
+    // language=RegExp
+    var ex = "[0-9A-Za-z ]*";
 })();
