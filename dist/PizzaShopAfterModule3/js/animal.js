c9eea814e28376ee49d578d1150a1ef08437263e
YonePC <YonePC@YONE>
2017-10-07 15:52:25 +0100

    Using debugger for second time in WS: You can write into the consoloe window.location.toString() to see where we are, we can right click a variable and set its value during debugging, we got informed about what is babel because of an uncaught error about not having referenced animal class which in fact is into index.html as a script

---
+++ dist/PizzaShopAfterModule3/js/animal.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var Animal = function Animal() {
+  this.name = "";
+  this.legCount = 0;
+  this.feed = function (food) {
+    console.log(this.name + " fed " + food);
+  };
+};
+//# sourceMappingURL=animal.js.map
---
+++ dist/PizzaShopAfterModule3/js/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// index.js
+(function () {
+    "use strict";
+
+    /**
+     * Submits an order
+     * @param parameters
+     */
+
+    function submitOrder(parameters) {
+        var validate = parameters.validate;
+        var size = parameters.size;
+        var crust = parameters.crust;
+        var topping = parameters.topping;
+        alert("not implemented");
+    }
+
+    $(document).ready(function () {
+        $("#orderForm").on("submit", function () {
+            submitOrder({ validate: false, size: "large", crust: "thin", topping: ["pepperoni", "sausage"] });
+            return false;
+        });
+    });
+
+    var dog = new Animal();
+    dog.name = "Lucky";
+    dog.legCount = 3;
+    dog.feed("Fish");
+
+    var toInsert = "<div>\n    <ul>\n        <li>Lorem ipsum dolor.</li>\n        <li>Blanditiis, explicabo, tempora.</li>\n        <li>Commodi, distinctio, labore?</li>\n        <li>Hic, obcaecati, ratione.</li>\n        <li>Id, incidunt, temporibus.</li>\n    </ul>\n</div>";
+
+    // language=JavaScript
+    var toExecute = "var x = 0;\nx++;\nx = 5;";
+
+    // language=RegExp
+    var ex = "[0-9A-Za-z ]*";
+})();
+//# sourceMappingURL=index.js.map
