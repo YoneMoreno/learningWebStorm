d247b0e07c533713e00f5c610f968ab834ce3eb2
YonePC <YonePC@YONE>
2017-10-07 17:53:13 +0100

    Creating our first server in nodejs

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,11 @@
 "use strict";
 
-console.log("Hello from NodeJS");
+var http = require("http");
+
+var server = http.createServer(function (req, res) {
+    res.write("<html>\n<body><h1>Hello from Webstorm</h1></body>\n</html>");
+    res.end();
+});
+
+server.listen(3000);
 //# sourceMappingURL=server.js.map
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Hello from NodeJS");
+var http = require("http");
+
+var server = http.createServer(function (req, res) {
+    res.write("<html>\n<body><h1>Hello from Webstorm</h1></body>\n</html>")
+    res.end();
+});
+
+server.listen(3000);
